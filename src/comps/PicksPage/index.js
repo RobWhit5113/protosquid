9ef4d80cabba0f5a52338eaff0e12a9280387df2
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-12-13 07:02:41 -0500

    updated for day 2

--- src/comps/PicksPage/index.js
+++ src/comps/PicksPage/index.js
@@ -5,8 +5,8 @@ import { Card, Button, DropdownButton, Dropdown } from 'react-bootstrap';
 import { send } from 'emailjs-com'
 
 function PicksPage() {
-  const [picksVal, setPicksVal] = useState("Choose Game");
-  const [picksVal2, setPicksVal2] = useState("Over/Under");
+  const [picksVal, setPicksVal] = useState("Choose Winner");
+  // const [picksVal2, setPicksVal2] = useState("Over/Under");
   // const [picksVal3, setPicksVal3] = useState("Please Choose a Team");
   const [email, setEmail] = useState('')
   const [number, setNumber] = useState(0)
@@ -27,26 +27,26 @@ function PicksPage() {
   const handlePicksEntry = async () => {
 
     try {
-      if (picksVal !== "Choose Game" && picksVal2 !== "Over/Under"){
+      if (picksVal !== "Choose Game"){
       await db('ENTRIES').insert({
         email: email,
         squidnumber: number,
         picks: picksVal,
-        picks2: picksVal2,
+        // picks2: picksVal2,
         // picks3: picksVal3,
         createdAt: moment().format('MM-DD-YYYY HH:mm:ss'),
       }).one()
 
       const emailParams = {
         picksVal,
-        picksVal2,
+        // picksVal2,
         email
       }
 
       send('service_bzmlyac', 'template_7llr3mp', emailParams, 'user_O754s7DH7re8j3oRghLIE' )
 
       setPicksVal("");
-      setPicksVal2("");
+      // setPicksVal2("");
       setStep(1)} 
       
       else {
@@ -69,9 +69,9 @@ function PicksPage() {
   setPicksVal(e)
   }
 
-  const handleSelect2 = e => {
-  setPicksVal2(e)
-  }
+  // const handleSelect2 = e => {
+  // setPicksVal2(e)
+  // }
   // const handleSelect3 = e => {
   // setPicksVal3(e)
   // }
@@ -88,80 +88,98 @@ function PicksPage() {
   if(step == 0){
     action = 
     ///////////////////////////////////day 1//////////////////////////////////////////////////////////////////
+      // <>
+      //   <div className='page-title'>
+      //     <h1 className='title' > Welcome Player #{number}</h1>
+      //   </div>
+      //   <div className='header'>
+      //     <div className='page-players-left'>
+      //       <Card className='players-left-card'>
+      //         <Card.Body>38 Players Left</Card.Body>
+      //       </Card>
+      //     </div>
+      //     <div className='page-jackpot'>
+      //       <Card className='jackpot-card'>
+      //         <Card.Body>Jackpot: $1,000</Card.Body>
+      //       </Card>
+      //     </div>
+      //   </div>
+      //   <div className='directions-container'>
+      //       <h3 className='directions'>Please enter your selection for Sunday's O/U NFL Game: </h3>
+      //   </div>
+      //   <div className='picks-container'>  
+      //   <div className='directions-container'>
+      //       <h5 className='directions'>Please Choose a Game Here: </h5>
+      //   </div>
+      //     <div id='team-pick'className="dropdown-selector">
+      //       <DropdownButton id="dropdown-basic-button1" title={picksVal} onSelect={handleSelect} size='lg'>
+      //         <Dropdown.Item eventKey='BAL v CLE (42.5)'>BAL v CLE (42.5)</Dropdown.Item>
+      //         <Dropdown.Item eventKey='JAC v TEN (44)'>JAC v TEN (44)</Dropdown.Item>
+      //         <Dropdown.Item eventKey='LA v KC (48.5)'>LV v KC (48.5)</Dropdown.Item>
+      //         <Dropdown.Item eventKey='NO v NYJ (43.5)'>NO v NYJ (43.5)</Dropdown.Item>
+      //         <Dropdown.Item eventKey='DAL v WAS (48.5)'>DAL v WAS (48.5)</Dropdown.Item>
+      //         <Dropdown.Item eventKey='ATL v CAR (42.5)'>ATL v CAR (42.5)</Dropdown.Item>
+      //         <Dropdown.Item eventKey='SEA v HOU (41.5)'>SEA v HOU (41.5)</Dropdown.Item>
+      //         <Dropdown.Item eventKey='DET v DEN (42)'>DET v DEN (42)</Dropdown.Item>
+      //         <Dropdown.Item eventKey='NYG v LAC (44.5)'>NYG v LAC (44.5)</Dropdown.Item>
+      //         <Dropdown.Item eventKey='SF v CIN (47.5)'>SF v CIN (47.5)</Dropdown.Item>
+      //         <Dropdown.Item eventKey='BUF v TB (53)'>BUF v TB (53)</Dropdown.Item>
+      //       </DropdownButton>
+      //     </div>
+      //     <div className='directions-container'>
+      //       <h5 className='directions'>Please Choose O/U Here: </h5>
+      //   </div>
+      //     <div id='over-pick'className="dropdown-selector">
+      //       <DropdownButton id="dropdown-basic-button2" title={picksVal2} onSelect={handleSelect2} size='lg'>
+      //         <Dropdown.Item eventKey='Over'>Over</Dropdown.Item>
+      //         <Dropdown.Item eventKey='Under'>Under</Dropdown.Item>
+      //       </DropdownButton>
+      //     </div>
+      //     <div className='enter-picks-button-container'>
+      //       <Button variant='primary' id='button' onClick={handlePicksEntry} size='lg'>Enter Picks</Button>
+      //     </div>
+      //   </div>
+      // </>
+      ///////////////////////////////////day 2//////////////////////////////////////////////////////////////////
       <>
         <div className='page-title'>
           <h1 className='title' > Welcome Player #{number}</h1>
         </div>
-        <div className='header'>
-          <div className='page-players-left'>
-            <Card className='players-left-card'>
-              <Card.Body>38 Players Left</Card.Body>
-            </Card>
+          <div className='header'>
+            <div className='page-players-left'>
+              <Card className='players-left-card'>
+                <Card.Body>38 Players Left</Card.Body>
+              </Card>
+            </div>
+            <div className='page-jackpot'>
+              <Card className='jackpot-card'>
+                <Card.Body>Jackpot: $1,000</Card.Body>
+              </Card>
+            </div>
           </div>
-          <div className='page-jackpot'>
-            <Card className='jackpot-card'>
-              <Card.Body>Jackpot: $1,000</Card.Body>
-            </Card>
-          </div>
-        </div>
         <div className='directions-container'>
-            <h3 className='directions'>Please enter your selection for Sunday's O/U NFL Game: </h3>
+          <h3 className='directions'>Please enter your selection for Monday's MNF Winner: </h3>
         </div>
-        <div className='picks-container'>  
-        <div className='directions-container'>
-            <h5 className='directions'>Please Choose a Game Here: </h5>
-        </div>
+        <div className='picks-container'>
           <div id='team-pick'className="dropdown-selector">
             <DropdownButton id="dropdown-basic-button1" title={picksVal} onSelect={handleSelect} size='lg'>
-              <Dropdown.Item eventKey='BAL v CLE (42.5)'>BAL v CLE (42.5)</Dropdown.Item>
-              <Dropdown.Item eventKey='JAC v TEN (44)'>JAC v TEN (44)</Dropdown.Item>
-              <Dropdown.Item eventKey='LA v KC (48.5)'>LV v KC (48.5)</Dropdown.Item>
-              <Dropdown.Item eventKey='NO v NYJ (43.5)'>NO v NYJ (43.5)</Dropdown.Item>
-              <Dropdown.Item eventKey='DAL v WAS (48.5)'>DAL v WAS (48.5)</Dropdown.Item>
-              <Dropdown.Item eventKey='ATL v CAR (42.5)'>ATL v CAR (42.5)</Dropdown.Item>
-              <Dropdown.Item eventKey='SEA v HOU (41.5)'>SEA v HOU (41.5)</Dropdown.Item>
-              <Dropdown.Item eventKey='DET v DEN (42)'>DET v DEN (42)</Dropdown.Item>
-              <Dropdown.Item eventKey='NYG v LAC (44.5)'>NYG v LAC (44.5)</Dropdown.Item>
-              <Dropdown.Item eventKey='SF v CIN (47.5)'>SF v CIN (47.5)</Dropdown.Item>
-              <Dropdown.Item eventKey='BUF v TB (53)'>BUF v TB (53)</Dropdown.Item>
+              <Dropdown.Item eventKey='LAR'>LAR</Dropdown.Item>
+              <Dropdown.Item eventKey='ARI'>ARI</Dropdown.Item>
             </DropdownButton>
           </div>
-          <div className='directions-container'>
-            <h5 className='directions'>Please Choose O/U Here: </h5>
-        </div>
-          <div id='over-pick'className="dropdown-selector">
-            <DropdownButton id="dropdown-basic-button2" title={picksVal2} onSelect={handleSelect2} size='lg'>
-              <Dropdown.Item eventKey='Over'>Over</Dropdown.Item>
-              <Dropdown.Item eventKey='Under'>Under</Dropdown.Item>
-            </DropdownButton>
-          </div>
+          {/* CHANGE OVER CHECKLIST
+              comment out uneeded picksVals
+              Check state values
+              Check the submit handler
+              Run a test pick
+              Run a false test pick
+              Change Email 
+              Check the email picks*/}
           <div className='enter-picks-button-container'>
             <Button variant='primary' id='button' onClick={handlePicksEntry} size='lg'>Enter Picks</Button>
           </div>
         </div>
       </>
-      ///////////////////////////////////day 2//////////////////////////////////////////////////////////////////
-      // <div className='picks-container'>
-      //   <div className='directions-container'>
-      //     <h3 className='directions'>Please enter your selection for Monday's MNF Winner: </h3>
-      //   </div>
-      //   <div id='team-pick'className="dropdown-selector">
-      //     <DropdownButton id="dropdown-basic-button1" title={picksVal} onSelect={handleSelect} size='lg'>
-      //       <Dropdown.Item eventKey='LAR'>LAR</Dropdown.Item>
-      //       <Dropdown.Item eventKey='ARI'>ARI</Dropdown.Item>
-      //     </DropdownButton>
-      //   </div>
-      //   {/* CHANGE OVER CHECKLIST
-      //       comment out uneeded picksVals
-      //       Check state values
-      //       Check the submit handler
-      //       Run a test pick
-      //       Run a false test pick 
-      //       Check the email picks*/}
-      //   <div className='button-container'>
-      //     <Button variant='primary' id='button' onClick={handlePicksEntry}>Enter Picks</Button>
-      //   </div>
-      // </div>
       ///////////////////////////////////day 3//////////////////////////////////////////////////////////////////
       // <div className='picks-container'>
       //   <div className='directions-container'>
