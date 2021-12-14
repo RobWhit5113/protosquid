6bf3c10999fb07104a90eb7b92f2aaa8cefa62c6
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-12-14 09:10:08 -0500

    NBA Day

--- src/comps/PicksPage/index.js
+++ src/comps/PicksPage/index.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect, useRef} from 'react';
 import { useEasybase } from "easybase-react";
 import moment from 'moment'
-import { Card, Button, DropdownButton, Dropdown } from 'react-bootstrap';
+import { Card, Button, DropdownButton, Dropdown, ListGroup } from 'react-bootstrap';
 import { send } from 'emailjs-com'
 
 function PicksPage() {
   const [picksVal, setPicksVal] = useState("Choose Winner");
-  // const [picksVal2, setPicksVal2] = useState("Over/Under");
+  // const [picksVal2, setPicksVal2] = useState("Choose Winner");
   // const [picksVal3, setPicksVal3] = useState("Please Choose a Team");
+  
   const [email, setEmail] = useState('')
   const [number, setNumber] = useState(0)
   const [step, setStep] = useState(0)
@@ -65,8 +66,9 @@ function PicksPage() {
   }
 
   //picks entry
-  const handleSelect = e => {
-  setPicksVal(e)
+  const handleSelect = (e) => {
+   setPicksVal(e)
+  
   }
 
   // const handleSelect2 = e => {
@@ -141,89 +143,105 @@ function PicksPage() {
       //   </div>
       // </>
       ///////////////////////////////////day 2//////////////////////////////////////////////////////////////////
-      <>
-        <div className='page-title'>
-          <h1 className='title' > Welcome Player #{number}</h1>
-        </div>
-          <div className='header'>
-            <div className='page-players-left'>
-              <Card className='players-left-card'>
-                <Card.Body>38 Players Left</Card.Body>
-              </Card>
-            </div>
-            <div className='page-jackpot'>
-              <Card className='jackpot-card'>
-                <Card.Body>Jackpot: $1,000</Card.Body>
-              </Card>
-            </div>
-          </div>
-        <div className='directions-container'>
-          <h3 className='directions'>Please enter your selection for Monday's MNF Winner: </h3>
-        </div>
-        <div className='picks-container'>
-          <div id='team-pick'className="dropdown-selector">
-            <DropdownButton id="dropdown-basic-button1" title={picksVal} onSelect={handleSelect} size='lg'>
-              <Dropdown.Item eventKey='LAR'>LAR</Dropdown.Item>
-              <Dropdown.Item eventKey='ARI'>ARI</Dropdown.Item>
-            </DropdownButton>
-          </div>
-          {/* CHANGE OVER CHECKLIST
-              comment out uneeded picksVals
-              Check state values
-              Check the submit handler
-              Run a test pick
-              Run a false test pick
-              Change Email 
-              Check the email picks*/}
-          <div className='enter-picks-button-container'>
-            <Button variant='primary' id='button' onClick={handlePicksEntry} size='lg'>Enter Picks</Button>
-          </div>
-        </div>
-      </>
-      ///////////////////////////////////day 3//////////////////////////////////////////////////////////////////
-      // <div className='picks-container'>
+      // <>
+      //   <div className='page-title'>
+      //     <h1 className='title' > Welcome Player #{number}</h1>
+      //   </div>
+      //     <div className='header'>
+      //       <div className='page-players-left'>
+      //         <Card className='players-left-card'>
+      //           <Card.Body>17 Players Left</Card.Body>
+      //         </Card>
+      //       </div>
+      //       <div className='page-jackpot'>
+      //         <Card className='jackpot-card'>
+      //           <Card.Body>Jackpot: $1,000</Card.Body>
+      //         </Card>
+      //       </div>
+      //     </div>
       //   <div className='directions-container'>
-      //     <h3 className='directions'>Please enter your selection for Wednesday's 3-Leg Bball Parlay: </h3>
+      //     <h3 className='directions'>Please enter your selection for Monday's MNF Winner: </h3>
       //   </div>
-      //   <div id='team-pick'className="dropdown-selector">
-      //      <DropdownButton id="dropdown-basic-button1" title={picksVal} onSelect={handleSelect} size='lg'>
-      //        <Dropdown.Item eventKey='BAL v CLE (42.5)'>BAL v CLE (42.5)</Dropdown.Item>
-      //        <Dropdown.Item eventKey='JAC v TEN (44)'>JAC v TEN (44)</Dropdown.Item>
-      //        <Dropdown.Item eventKey='LA v KC (48.5)'>LV v KC (48.5)</Dropdown.Item>
-      //        <Dropdown.Item eventKey='NO v NYJ (43.5)'>NO v NYJ (43.5)</Dropdown.Item>
-      //        <Dropdown.Item eventKey='DAL v WAS (48.5)'>DAL v WAS (48.5)</Dropdown.Item>
-      //        <Dropdown.Item eventKey='ATL v CAR (42.5)'>ATL v CAR (42.5)</Dropdown.Item>
-      //        <Dropdown.Item eventKey='SEA v HOU (41.5)'>SEA v HOU (41.5)</Dropdown.Item>
-      //        <Dropdown.Item eventKey='DET v DEN (42)'>DET v DEN (42)</Dropdown.Item>
-      //        <Dropdown.Item eventKey='NYG v LAC (44.5)'>NYG v LAC (44.5)</Dropdown.Item>
-      //        <Dropdown.Item eventKey='SF v CIN (47.5)'>SF v CIN (47.5)</Dropdown.Item>
-      //        <Dropdown.Item eventKey='BUF v TB (53)'>BUF v TB (53)</Dropdown.Item>
-      //      </DropdownButton>
-      //    </div>
-      //    <div id='over-pick'className="dropdown-selector">
-      //      <DropdownButton id="dropdown-basic-button2" title={picksVal2} onSelect={handleSelect2} size='lg'>
-      //        <Dropdown.Item eventKey='Over'>Over</Dropdown.Item>
-      //        <Dropdown.Item eventKey='Under'>Under</Dropdown.Item>
-      //      </DropdownButton>
-      //    </div>
-      //    <div id='over-pick'className="dropdown-selector">
-      //      <DropdownButton id="dropdown-basic-button2" title={picksVal3} onSelect={handleSelect3} size='lg'>
-      //        <Dropdown.Item eventKey='Over'>Over</Dropdown.Item>
-      //        <Dropdown.Item eventKey='Under'>Under</Dropdown.Item>
-      //      </DropdownButton>
-      //    </div>
-      //   {/* CHANGE OVER CHECKLIST
-      //       comment out uneeded picksVals
-      //       Check state values
-      //       Check the submit handler
-      //       Run a test pick
-      //       Run a false test pick 
-      //       Check the email picks*/}
-      //   <div className='button-container'>
-      //     <Button variant='primary' id='button' onClick={handlePicksEntry}>Enter Picks</Button>
+      //   <div className='picks-container'>
+      //     <div id='team-pick'className="dropdown-selector">
+      //       <DropdownButton id="dropdown-basic-button1" title={picksVal} onSelect={handleSelect} size='lg'>
+      //         <Dropdown.Item eventKey='LAR'>LAR</Dropdown.Item>
+      //         <Dropdown.Item eventKey='ARI'>ARI</Dropdown.Item>
+      //       </DropdownButton>
+      //     </div>
+      //     {/* CHANGE OVER CHECKLIST
+      //         comment out uneeded picksVals
+      //         Check state values
+      //         Check the submit handler
+      //         Run a test pick
+      //         Run a false test pick
+      //         Change Email 
+      //         Check the email picks*/}
+      //     <div className='enter-picks-button-container'>
+      //       <Button variant='primary' id='button' onClick={handlePicksEntry} size='lg'>Enter Picks</Button>
+      //     </div>
       //   </div>
-      // </div>
-      ///////////////////////////////////day 4//////////////////////////////////////////////////////////////////
+      // </>
+      ///////////////////////////////////day 3//////////////////////////////////////////////////////////////////
+    <>
+      
+      <div className='page-title'>
+        <h1 className='title' > Welcome Player #{number}</h1>
+       </div>
+       <div className='header'>
+        <div className='page-players-left'>
+          <Card className='players-left-card'>
+            <Card.Body>6 Players Left</Card.Body>
+          </Card>
+        </div>
+        <div className='page-jackpot'>
+          <Card className='jackpot-card'>
+            <Card.Body>Jackpot: $1,000</Card.Body>
+          </Card>
+        </div>
+      </div>
+      <div className='directions2-container'>
+        <h5 className='directions'>NBA Games Tonight:</h5>
+        <ListGroup horizontal={'sm'} className='games-list'>
+          <ListGroup.Item>TOR v BKN</ListGroup.Item>
+          <ListGroup.Item>GS v NYK</ListGroup.Item>
+          <ListGroup.Item>PHX v POR</ListGroup.Item>
+        </ListGroup>
+     </div>
+      <div className='directions-container'>
+        <h3 className='directions'>Please enter your selection for Wednesday's NBA Winner: </h3>
+     </div>
+      <div className='picks-container'>
+        <div id='team-pick'className="dropdown-selector">
+           <DropdownButton id="dropdown-basic-button1" title={picksVal} onSelect={handleSelect} size='lg'>
+             <Dropdown.Item eventKey='TOR'>TOR</Dropdown.Item>
+             <Dropdown.Item eventKey='BKN'>BKN</Dropdown.Item>
+             <Dropdown.Item eventKey='GS'>GS</Dropdown.Item>
+             <Dropdown.Item eventKey='NYK'>NYK</Dropdown.Item>
+             <Dropdown.Item eventKey='PHX'>PHX</Dropdown.Item>
+             <Dropdown.Item eventKey='POR'>POR</Dropdown.Item>
+           </DropdownButton>
+         </div>
+         {/* MIGHT JUST BE ONE SELECTION  */}
+         {/* <div id='over-pick'className="dropdown-selector">
+           <DropdownButton id="dropdown-basic-button2" title={picksVal2} onSelect={handleSelect2} size='lg'>
+             <Dropdown.Item eventKey={pickChoice1}>Over</Dropdown.Item>
+             <Dropdown.Item eventKey={pickChoice2}>Under</Dropdown.Item>
+           </DropdownButton>
+         </div> */}
+        {/* CHANGE OVER CHECKLIST
+            comment out uneeded picksVals
+            Check state values
+            Check the submit handler
+            Run a test pick
+            Run a false test pick 
+            Check the email picks*/}
+        <div className='enter-picks-button-container'>
+          <Button variant='primary' id='button' onClick={handlePicksEntry}>Enter Picks</Button>
+        </div>
+      </div>
+    </>
+      /* ///////////////////////////////////day 4//////////////////////////////////////////////////////////////////
       // <div className='picks-container'>
       //   <div className='directions-container'>
       //     <h3 className='directions'>Please enter your selection for TNF Anytime TD Scorer: </h3>
@@ -253,11 +271,11 @@ function PicksPage() {
       //       Check the submit handler
       //       Run a test pick
       //       Run a false test pick 
-      //       Check the email picks*/}
-      //   <div className='button-container'>
+      //       Check the email picks*/
+      /* //   <div className='button-container'>
       //     <Button variant='primary' id='button' onClick={handlePicksEntry}>Enter Picks</Button>
       //   </div>
-      // </div>
+      // </div> */
       
   }else if (step == 1){
     action = 
