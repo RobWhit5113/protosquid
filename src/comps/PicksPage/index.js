5cdcfbb2c46e70eb384ce79f7d3d6fa487140f04
Rob Whitacre <robert.w.whitacre@gmail.com>
2021-12-19 11:57:54 -0500

    final adjustments

--- src/comps/PicksPage/index.js
+++ src/comps/PicksPage/index.js
@@ -330,7 +330,7 @@ function PicksPage() {
       <div className='header'>
         <div className='page-players-left'>
           <Card className='players-left-card'>
-            <Card.Body>6 Players Left</Card.Body>
+            <Card.Body>1 Player Left</Card.Body>
           </Card>
         </div>
         <div className='page-jackpot'>
